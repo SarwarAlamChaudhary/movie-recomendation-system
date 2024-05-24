229d500d5a92c8b343ab71d8880e15b0569bc7ba
Aalam2023 <sarwarchaudhary@dpcoepune.edu.in>
2024-05-24 10:49:35 +0530

    First commit

---
+++ src/utils/constant.js
@@ -0,0 +1,17 @@
+export const API_END_POINT = "http://localhost:8080/api/v1/user";
+
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjM2MzAxODk4OTlmZDlkMmZkZGEwM2Y1YzJhYmFhMCIsInN1YiI6IjY2NDhkZTg0N2UzZjJkMTczNzY1ZDY0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y4_Zup1Ww-Wcr7ac5kn2sktl2-D_cqql5v_qA010uU0'
+  }
+};
+export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing";
+export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
+export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
+export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
+
+export const  SEARCH_MOVIE_URL="https://api.themoviedb.org/3/search/movie?query=";
+
+export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";
